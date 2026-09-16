c2441bc1d697b8eece49c373099276dd8698b20a
Kristina Ousley <kristinaousley@Kristinas-MacBook-Pro.local>
2026-09-16 10:55:53 -0400

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base:'/portfolio/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
@@ -23,5 +24,5 @@ export default defineConfig(({mode}) => {
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
-   base:'/portfolio/'
+   
 });
